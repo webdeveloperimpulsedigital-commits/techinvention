2f866667c2b9d842ef9ef3dd7bd7772ecf1d6f36
shwetakhunte <shweta.khunte23@gmail.com>
2026-04-01 16:23:58 +0530

    config chnages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/techinvention/',
+    base: '/Techinvention/',
     plugins: [react()],
     resolve: {
         alias: {
